0867ab1974842a93d20cc6ce2292bb3657ac001f
wei chen <weichen9083@gmail.com>
2019-09-21 01:14:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash.each do |key, value|
     if key == season
       key[holiday_name] = supply_array
-      binding.pry
+      
     end 
 end 
 end
