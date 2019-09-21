d471037689792726ed393e58a2e0dba5792a1323
wei chen <weichen9083@gmail.com>
2019-09-21 00:50:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |key , value| 
     if key == :winter 
-
+        value.each do |attribute, what|
+          attribute << supply
+        end 
     end 
   end 
 
