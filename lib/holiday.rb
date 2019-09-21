4e4b53dd304f00f3600ca29db43353055456042b
wei chen <weichen9083@gmail.com>
2019-09-21 01:40:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |key, value|
+  
+end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
