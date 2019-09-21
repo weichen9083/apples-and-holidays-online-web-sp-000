639a33127da171b413bba99ac532771046aad8ee
wei chen <weichen9083@gmail.com>
2019-09-21 00:57:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,6 +41,15 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+   holiday_hash.each do |key , value| 
+    if key == :spring 
+        value.each do |attribute, what|
+         what << supply
+
+        end 
+
+    end 
+  end 
 
 end
 
