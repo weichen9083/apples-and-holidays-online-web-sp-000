386ac968da560ed1f1666e6c09f2380d60cdb0ac
wei chen <weichen9083@gmail.com>
2019-09-21 01:47:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,8 +79,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |key, value|
-  puts "#{key} #{value}"
-  key.capitalize
+puts "#{key.to_a.capitalize!}:"
 end 
 end
 
